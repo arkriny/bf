a2ceb9f02cab751285b06af155020c9f1f3df8ea
Artem Krinitsyn <arkriny@gmail.com>
2026-06-23 13:03:11 +0000

    bfi: add initial interpreter implementaion

---
+++ bfi.c
@@ -0,0 +1,101 @@
+/* bfi - brainfuck interpreter */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char tape[65536];
+char src[1 << 20];
+size_t src_size, head;
+int balance;
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "bf: no input file\n");
+		return 2;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+	if (!fp) {
+		fprintf(stderr, "bf: open %s: %s\n", argv[1], strerror(errno));
+		return 1;
+	}
+	src_size = fread(src, 1, sizeof(src), fp);
+	fclose(fp);
+
+	for (size_t i = 0; i < src_size; ++i) {
+		switch (src[i]) {
+			case '>':
+				if (head == sizeof(tape) - 1) {
+					fprintf(stderr, "bf: max tape size %zu exceeded\n", sizeof(tape));
+					return 1;
+				}
+				++head;
+				break;
+			case '<':
+				if (head == 0) {
+					fprintf(stderr, "bf: tape pointer underflow\n");
+					return 1;
+				}
+				--head;
+				break;
+			case '+':
+				++tape[head];
+				break;
+			case '-':
+				--tape[head];
+				break;
+			case '.':
+				putchar(tape[head]);
+				break;
+			case ',':
+				tape[head] = getchar();
+				break;
+			case '[':
+				if (tape[head] != 0) {
+					break;
+				}
+				balance = 1;
+				++i;
+				for (; src[i] < src_size; ++i) {
+					if (src[i] == ']') {
+						--balance;
+						if (balance == 0) {
+							break;
+						}
+					} else if (src[i] == '[') {
+						++balance;
+					}
+				}
+				if (i == src_size) {
+					fprintf(stderr, "bf: unmatched [\n");
+					return 1;
+				}
+				break;
+			case ']':
+				if (tape[head] == 0) {
+					break;
+				}
+				balance = 1;
+				for (; i > 0; --i) {
+					if (src[i-1] == '[') {
+						--balance;
+						if (balance == 0) {
+							break;
+						}
+					} else if (src[i-1] == ']') {
+						++balance;
+					}
+				}
+				if (i == 0) {
+					fprintf(stderr, "bf: unmatched ]\n");
+					return 1;
+				}
+				--i;
+				break;
+		}
+	}
+	return 0;
+}
